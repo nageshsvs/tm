a41ec0eb4a4dd2e91895cbfbe9cc8d2a12ecc35f
nageshsvs <34898612+nageshsvs@users.noreply.github.com>
2019-09-03 14:41:44 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(name='timeseries',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/nageshsvs/timeseries/blob/master/time_series_nagesh.py',
+      dependency_links=['https://github.com/nageshsvs/timeseries/blob/master#egg=package-1.0'],
+      author='Nagesh Somayajula',
+      author_email='Nagesh.Somayajula@hitaciVantara.com',
+      license='EDP',
+      packages=['TimeSeries'],
+      install_requires=[
+          'statsmodels','sklearn',
+      ],
+      zip_safe=False)
